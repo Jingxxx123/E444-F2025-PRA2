ea7db1c0abaf78a7b4f47b1ee4e004f5a9248af3
Jingqiao Xiao <jenniferxjq@gmail.com>
2025-09-20 22:54:57 -0400

    add current time on page

--- hello3-3.py
+++ hello3-3.py
@@ -1,17 +1,20 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
 bootrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    return render_template('user.html', name='World')
+    return render_template('index.html', current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', name=name)
+    return render_template('user.html', name=name, current_time=datetime.utcnow())
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -21,6 +24,8 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
 
